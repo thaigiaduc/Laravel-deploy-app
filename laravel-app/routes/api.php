c6d5cb658ba16561be4bcbb2b3db021aa6029440
Peter <peter@artistsworld.me>
2024-01-03 00:40:47 +0700

    fixed api kernel and kubernetes ingress

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function () {
-    return "hello world";
+Route::get('/test-api', function () {
+    return response()->json(['message' => 'Hello from API']);
 });
